823b03846273f03a2931706e215ff9409816ef45
Aheras <taha-aheras.mohd-zahid@capgemini.com>
2026-02-04 17:14:25 +0530

    Add CI/CD workflows-9

--- bin/app-pipeline.ts
+++ bin/app-pipeline.ts
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { AppStack } from '../lib/app-stack';
-import { AppPipelineStack } from '../lib/app-pipeline-stack';
 
 const app = new cdk.App();
 
 // Instantiate both stacks
-new AppPipelineStack(app, 'AppPipelineStack');
 new AppStack(app, 'AppStack', { 
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
