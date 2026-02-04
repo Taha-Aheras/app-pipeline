573774ba129820813a3e3e8226ad3c8a7fa598d2
Aheras <taha-aheras.mohd-zahid@capgemini.com>
2026-02-04 15:02:22 +0530

    Add CI/CD workflows-7

--- bin/app-pipeline.ts
+++ bin/app-pipeline.ts
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { AppStack } from '../lib/app-stack';
+import { AppPipelineStack } from '../lib/app-pipeline-stack';
 
 const app = new cdk.App();
 
+// Instantiate both stacks
+new AppPipelineStack(app, 'AppPipelineStack');
 new AppStack(app, 'AppStack', { 
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
 });
--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,12 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, 'HelloFn', {
+    const helloFn = new lambda.Function(this, 'HelloFn', {
       runtime: lambda.Runtime.NODEJS_20_X,
       handler: 'index.handler',
       code: lambda.Code.fromInline(`
@@ -14,6 +15,17 @@ export class AppStack extends cdk.Stack {
           return { statusCode: 200, body: "Hello from CDK!" };
         };
       `),
+    });
+
+    // REST API for Lambda
+    const api = new apigw.LambdaRestApi(this, 'HelloApi', {
+      handler: helloFn,
+      proxy: true
+    });
+
+    // Output the URL so GitHub Actions can read it
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: api.url,
     });
   }
 }
