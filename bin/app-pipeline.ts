1fc4e059095cbe91bd5097fd3151583205ecaadc
Aheras <taha-aheras.mohd-zahid@capgemini.com>
2026-02-04 12:51:13 +0530

    Add CI/CD workflows-4

--- bin/app-pipeline.ts
+++ bin/app-pipeline.ts
@@ -1,15 +1,9 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { AppStack } from '../lib/app-stack';
-import { GitHubOidcStack } from '../lib/github-oidc-stack';
 
 const app = new cdk.App();
 
-// 1) OIDC stack (creates IdP + role for Actions)
-new GitHubOidcStack(app, 'GitHubOidcStack', {
-  // You can parameterize account/region via env if you like
+new AppStack(app, 'AppStack', { 
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
-
-// 2) App stack (Lambda + API)
-new AppStack(app, 'AppStack');
